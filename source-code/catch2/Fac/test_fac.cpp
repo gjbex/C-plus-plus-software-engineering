4b7149e5ce5d77a423506c9a611e54c659f3a495
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-05 13:46:20 +0100

    Split test case

--- source-code/catch2/Fac/test_fac.cpp
+++ source-code/catch2/Fac/test_fac.cpp
@@ -3,11 +3,26 @@
 
 #include "fac.h"
 
-TEST_CASE("factorials", "[fac]") {
+TEST_CASE("fac(0)", "[fac]") {
     REQUIRE( fac(0) == 1 );
+}
+
+TEST_CASE("fac(1)", "[fac]") {
     REQUIRE( fac(1) == 1 );
+}
+
+TEST_CASE("fac(3)", "[fac]") {
     REQUIRE( fac(3) == 6 );
+}
+
+TEST_CASE("fac(5)", "[fac]") {
     REQUIRE( fac(5) == 120 );
+}
+
+TEST_CASE("fac invalid argument", "[fac]") {
     REQUIRE_THROWS_AS( fac(-1), std::domain_error );
+}
+
+TEST_CASE("fac argument too large", "[fac]") {
     REQUIRE_THROWS_WITH( fac(20), "argument too large" );
 }
