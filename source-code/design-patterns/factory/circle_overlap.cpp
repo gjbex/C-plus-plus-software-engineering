fc0959f912cf56df5272e64e47b3d025e2c184d2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-05-22 22:16:32 +0200

    Simplify code

--- source-code/design-patterns/factory/circle_overlap.cpp
+++ source-code/design-patterns/factory/circle_overlap.cpp
@@ -6,13 +6,13 @@
 int main() {
     auto circles = Circle::read_circles();
     int nr_overlapping {0};
-    for (unsigned i = 0; i < circles.size(); ++i)
-        for (unsigned j = 0; j < circles.size(); ++j)
-            if (i != j)
-                if (circles[i].has_overlap(circles[j])) {
-                    ++nr_overlapping;
-                    break;
-                }
+    for (unsigned i = 0; i < circles.size(); ++i) {
+        for (unsigned j = i + 1; j < circles.size(); ++j) {
+            if (circles[i].has_overlap(circles[j])) {
+                ++nr_overlapping;
+            }
+        }
+    }
     std::cout << circles.size() << " " << nr_overlapping << std::endl;
     return 0;
 }
